901c231a4aa7f52ed86d9239d4bd1547b92a0a0c
Aili Zhang <aili.zhang91@gmail.com>
2017-07-21 12:49:57 +1000

    CB-2 Fix copy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,31 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :cashback_contents
+  before_action :base_url, :cashback_contents
 
   private
+
+  def base_url
+    @base_url = request.path # TODO: Need to be changed to request.base_url after getting domains
+  end
+
   def cashback_contents
-    if request.path.include?("nz") # change to request.base_url.include later
-      @home_page = nz_path # change to real NZ website
+    if @base_url.include?("nz") # TODO: Check base url
+      @home_page = nz_path # TODO: change to real NZ website
       @logo = "images/nz-logo.png"
       @logo_class = "nz-logo"
       @phone = "tel:0800669487"
       @phone_display = " 0800 669 487"
       @address = {line_1: "PO 147167", line_2: "Ponsonby, Auckland, 1144", line_3: ""}
+      @website = "Mycashback.co.nz"
     else
-      @home_page = au_path # change to real AU website
+      @home_page = au_path # TODO: change to real AU website
       @logo = "images/au-logo.png"
       @logo_class = "au-logo"
       @phone = "tel:1300669487"
       @phone_display = " 1 300 669 487"
       @address = {line_1: "PO BOX 6331", line_2: "Alexandria, NSW 2015", line_3: "Australia"}
+      @website = "Cashback.com.au"
     end
-
   end
 end
