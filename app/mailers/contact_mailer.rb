0b39e184b91a2ff2be485ac638bf30aef4f29dc5
Aili Zhang <aili.zhang91@gmail.com>
2017-07-25 17:27:12 +1000

    CB-6 Fix action mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,11 @@
 class ContactMailer < ApplicationMailer
-  def contact_cashback name, email, message
+  def contact_cashback contact_email, name, email, message
     @message = message
-    mail(from: email, to: @contact_email, subject: "CashBack - #{name}", body: @message)
+    mail(from: email, to: contact_email, subject: "CashBack - #{name}", body: @message)
   end
 
-  def contact_prologic name, email, message
+  def contact_prologic contact_email, name, email, message
     @message = message
-    mail(from: email, to: ENV['PROLOGIC_EMAIL'], subject: "ProLogic - #{name}", body: @message)
+    mail(from: email, to: contact_email, subject: "ProLogic - #{name}", body: @message)
   end
 end
