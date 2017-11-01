c2f0ba653d258be4cf3c72429f772fd9ba28b98a
Aili Zhang <>
2017-11-01 16:04:29 +1100

    CB-11 Update phone number and add international number

--- app/controllers/prologic_controller.rb
+++ app/controllers/prologic_controller.rb
@@ -1,6 +1,8 @@
 class PrologicController < ApplicationController
   layout "pro_logic"
-  # before_action :pro_logic_contects
+
+  def home
+  end
 
   def contact_prologic
     ContactMailer.contact_prologic(ENV['PROLOGIC_EMAIL'], params[:name], params[:email], params[:message]).deliver_later
